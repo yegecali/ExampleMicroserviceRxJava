704864d24de77f91cfdb25c888b29c6cb0da3e53
Yemi Genderson Canchari Lizarbe <39068001+yegecali@users.noreply.github.com>
2022-04-22 14:07:24 -0500

    feat: setup example retrofit

---
+++ MsOperation/src/main/java/com/yegecali/MsOperation/client/Api/AccountApi.java
@@ -0,0 +1,24 @@
+package com.yegecali.MsOperation.client.Api;
+
+import com.yegecali.MsOperation.client.dto.AccountDto;
+import io.reactivex.Completable;
+import io.reactivex.Single;
+import org.springframework.stereotype.Component;
+import retrofit2.http.*;
+
+import java.util.List;
+
+public interface AccountApi {
+    @GET("api/account")
+    Single<List<AccountDto>> listAccount();
+    @GET("api/account/{id}")
+    Single<AccountDto> getById(@Path("id") String id);
+    @GET("api/account/account-number/{accountnumber}")
+    Single<AccountDto> getByNumberAccount(@Path("accountnumber") String accountnumber);
+    @POST("api/account")
+    Single<AccountDto> addAccount(@Body AccountDto accountDto);
+    @PUT("api/account/{id}")
+    Completable updateAccount(@Body AccountDto accountDto, @Path("id") String id);
+    @DELETE("api/account/{id}")
+    Completable deleteAccount(@Path("id") String id);
+}
---
+++ MsOperation/src/main/java/com/yegecali/MsOperation/client/service/AccountService.java
@@ -0,0 +1,31 @@
+package com.yegecali.MsOperation.client.service;
+
+import com.netflix.discovery.converters.Auto;
+import com.yegecali.MsOperation.client.Api.AccountApi;
+import com.yegecali.MsOperation.client.dto.AccountDto;
+import io.reactivex.Single;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.List;
+
+@Service
+public class AccountService {
+
+    private AccountApi accountApi;
+
+    public AccountService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:8002/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build();
+        this.accountApi = retrofit.create(AccountApi.class);
+    }
+
+    public Single<List<AccountDto>> getAll(){
+        return accountApi.listAccount();
+    }
+}
