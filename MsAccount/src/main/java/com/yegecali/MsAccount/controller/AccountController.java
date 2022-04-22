84207cf52359c6c38a9de41dd528e285af1dc7b7
Yemi Genderson Canchari Lizarbe <39068001+yegecali@users.noreply.github.com>
2022-04-22 09:41:23 -0500

    feat: complete gateway

--- MsAccount/src/main/java/com/yegecali/MsAccount/controller/AccountController.java
+++ MsAccount/src/main/java/com/yegecali/MsAccount/controller/AccountController.java
@@ -7,6 +7,8 @@ import io.reactivex.Single;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/account")
 public class AccountController {
@@ -14,28 +16,33 @@ public class AccountController {
     @Autowired
     private IAccountService accountService;
 
+    @GetMapping
+    public Single<List<AccountDto>> getList() {
+        return accountService.getAll();
+    }
+
     @GetMapping("/{id}")
-    public Single<AccountDto> getById(@PathVariable("id") String id){
+    public Single<AccountDto> getById(@PathVariable("id") String id) {
         return accountService.getById(id);
     }
 
     @GetMapping("/account-number/{accountnumber}")
-    public Single<AccountDto> getbyNumberAccount(@PathVariable("accountnumber") String accountnumber){
+    public Single<AccountDto> getbyNumberAccount(@PathVariable("accountnumber") String accountnumber) {
         return accountService.findByNumberAccount(accountnumber);
     }
 
     @PostMapping
-    public Single<AccountDto> addAccount(@RequestBody AccountDto accountDto){
+    public Single<AccountDto> addAccount(@RequestBody AccountDto accountDto) {
         return accountService.add(accountDto);
     }
 
     @PutMapping("/{id}")
-    public Completable updateAccount(@RequestBody AccountDto accountDto, @PathVariable("id") String id){
+    public Completable updateAccount(@RequestBody AccountDto accountDto, @PathVariable("id") String id) {
         return accountService.update(accountDto, id);
     }
 
     @DeleteMapping("/{id}")
-    public Completable deleteAccountById(@PathVariable("id") String id){
+    public Completable deleteAccountById(@PathVariable("id") String id) {
         return accountService.deleteById(id);
     }
 
